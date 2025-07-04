0e6f49d832b637a751aa79daa627c9e5e7991010
nupirk <shahkripun@gmail.com>
2025-07-04 23:31:59 +0545

    fix(account):Circular relationship

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -1,15 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module,forwardRef } from '@nestjs/common';
 import { AccountService } from './account.service';
 import { AccountResolver } from './account.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Account } from './entities/account.entity';
 import { MobileOTP } from './entities/otp.entity';
-import { AuthService } from 'src/auth/auth.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports:[
-    TypeOrmModule.forFeature([Account,MobileOTP])
+    TypeOrmModule.forFeature([Account, MobileOTP]),
+    forwardRef(()=>AuthModule)
   ],
-  providers: [AccountResolver, AccountService,AuthService],
+  providers: [AccountResolver, AccountService],
+  exports:[AccountService]
 })
 export class AccountModule {}
